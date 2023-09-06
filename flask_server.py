b91d240114882ff9631a850e67bf658808a0b226
JasonHung <JasonHung@otsuka.com.tw>
2023-09-06 17:12:47 +0800

    update

--- flask_server.py
+++ flask_server.py
@@ -725,10 +725,13 @@ def production_2_work_check_record():
             #################
             # main content 
             #################
+            session['employee_id'] = db.search_item('employee_id' , user)
+            session['end_date']    = db.search_item('end_date' , user)
+
             factory_work_station = db.factory_work_station()
-            a_work_no            = db.search_item('employee_id' , user)
+            a_work_no            = session['employee_id']
             a_name               = session['user']
-            a_end_date           = db.search_item('end_date' , user)
+            a_end_date           = session['end_date']
             a_check_year         = db.search_member_item('check_year' , user)
             a_check_month        = db.search_member_item('check_month' , user)
             a_job_title          = db.factory_check_form_item(user)
